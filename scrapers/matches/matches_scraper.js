da1f1b76c7125e8b8004e11c9c363a752f363110
BrianJ-4 <brian.joshua03@gmail.com>
2025-03-25 10:40:10 -0700

    Complete getUpcomingAndLiveMatches

--- scrapers/matches/matches_scraper.js
+++ scrapers/matches/matches_scraper.js
@@ -1,11 +1,57 @@
-const { getPage } = require("../../utils/scrape_utils");
+const { getPage, getText } = require("../../utils/scrape_utils");
 const { getMainHeaderData, getStreamsAndVods, getStats } = require("../matches/helpers/get_match_information_helper");
 
 async function getUpcomingAndLiveMatches(page) {
-    const toReturn = {
-        function: "getUpcomingAndLiveMatches"
-    };
-    return toReturn;
+    const url = `https://vlr.gg/matches/?page=${page}`;
+    try {
+        // Access page and get HTML
+        const doc = await getPage(url);
+
+        let upcomingAndLiveMatches = {};
+        const dateSections = doc.querySelectorAll("div.wf-card[style]");
+        let dates = doc.querySelectorAll("div.wf-label.mod-large");
+        dates = dates.map(date => getText(date));
+        let i = 0;
+
+        dateSections.forEach(day => {
+            let k = 0;
+            const matches = day.children;
+            let dateMatches = {};
+
+            matches.forEach(match => {
+                dateMatches[k] = {
+                    ID: match.attributes["href"].match(/\/(\d+)\//)[1],
+                    Time: getText(match.querySelector("div.match-item-time")),
+                    TeamA: getText(match.querySelectorAll("div.match-item-vs-team-name")[0]),
+                    TeamB: getText(match.querySelectorAll("div.match-item-vs-team-name")[1]),
+                    Event: getText(match.querySelector("div.match-item-event.text-of").childNodes[2]),
+                    SubEvent: getText(match.querySelector("div.match-item-event.text-of").childNodes[1]),
+                    Status: getText(match.querySelector("div.ml-status"))
+                }
+                if (dateMatches[k].Status == "LIVE") {
+                    const scores = match.querySelectorAll("div.match-item-vs-team-score")
+                    dateMatches[k].TeamAScore = getText(scores[0]);
+                    dateMatches[k].TeamBScore = getText(scores[1]);
+                }
+                // Upcoming match
+                else {
+                    const timeUntil = getText(match.querySelector("div.ml-eta"));
+                    dateMatches[k].TimeUntil = timeUntil;
+                }
+                k += 1;
+            });
+            if (dates[i].includes("Today")) {
+                dates[i] = dates[i].substring(0, dates[i].indexOf("\n") - 1);
+            }
+            upcomingAndLiveMatches[dates[i]] = dateMatches
+            i += 1;
+        });
+        return upcomingAndLiveMatches;
+    } 
+    catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).json({ error: "Failed to fetch site data" });
+    }
 }
 
 async function getCompletedMatches() {
