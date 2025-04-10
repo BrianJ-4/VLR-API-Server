10c47d744e6239d38e9d30e74eae8b7ac63744ac
BrianJ-4 <brian.joshua03@gmail.com>
2025-04-10 10:49:36 -0700

    Add getPlayerResults and getPlayerTeams

--- scrapers/players/helpers/get_player_information_helper.js
+++ scrapers/players/helpers/get_player_information_helper.js
@@ -4,15 +4,15 @@ const { getText } = require("../../../utils/scrape_utils");
 
 function getMainHeaderData(mainHeader) {
     let playerDetails = {};
-    
+
     // Player image
     playerDetails.PlayerImage = mainHeader.querySelector("img").attributes.src;
-    
+
     // Player name and real name
     const playerDetailsSection = mainHeader.children[1];
     playerDetails.PlayerName = getText(playerDetailsSection.querySelector("h1.wf-title"));
     playerDetails.PlayerRealName = getText(playerDetailsSection.querySelector("h2.player-real-name"));
-    
+
     // Player socials links
     const socials = playerDetailsSection.querySelectorAll("a");
     let playerSocials = {};
@@ -27,7 +27,7 @@ function getMainHeaderData(mainHeader) {
 
     // Player nationality
     playerDetails.Nationality = getText(playerDetailsSection.querySelector("i.flag").nextSibling);
-    
+
     return playerDetails;
 }
 
@@ -44,7 +44,7 @@ function getPlayerAgentStats(statsTable) {
         agentInfo.Pick = getText(values[1]).split(" ")[1];
 
         const keys = [
-            "Rounds", "Rating", "ACS", "KD", "ADR", "KAST", "KPR", "APR", 
+            "Rounds", "Rating", "ACS", "KD", "ADR", "KAST", "KPR", "APR",
             "FKPR", "FDPR", "Kills", "Deaths", "Assists", "FKs", "FDs"
         ];
         keys.forEach((key, i) => {
@@ -55,4 +55,75 @@ function getPlayerAgentStats(statsTable) {
     return agentStats;
 }
 
-module.exports = { getMainHeaderData, getPlayerAgentStats };
+function getPlayerResults(resultsSection) {
+    let results = {};
+    const matches = resultsSection.children;
+
+    for (let i = 0; i < matches.length - 1; i++) { // Skips the last element since its something different
+        const match = matches[i];
+        let matchDetails = {};
+        const matchID = match.querySelector("a").attributes.href;
+
+        matchDetails.MatchID = matchID.slice(matchID.indexOf('/') + 1, matchID.indexOf('/', 1));
+
+        // Event details
+        matchDetails.EventImage = match.querySelector("div.fc-flex.m-item-thumb").querySelector("img").attributes.src;
+        eventNameContainer = match.querySelector("div.m-item-event").firstElementChild
+        matchDetails.EventName = getText(eventNameContainer);
+        eventNameContainer.remove();
+        matchDetails.SubEvent = getText(match.querySelector("div.m-item-event")).replace(/\s+/g, ' ');
+
+        // Team info
+        matchDetails.TeamAName = getText(match.querySelectorAll("span.m-item-team-name")[0]);
+        matchDetails.TeamBName = getText(match.querySelectorAll("span.m-item-team-name")[1]);
+        matchDetails.TeamAImage = match.querySelector("div.m-item-logo").querySelector("img").attributes.src;
+        matchDetails.TeamBImage = match.querySelector("div.m-item-logo.mod-right").querySelector("img").attributes.src;
+
+        // Score
+        const scoreContainer = match.querySelector("div.m-item-result");
+        matchDetails.TeamAScore = getText(scoreContainer.querySelectorAll("span")[0]);
+        matchDetails.TeamBScore = getText(scoreContainer.querySelectorAll("span")[1]);
+        if (scoreContainer.classList.contains("mod-win"))
+            matchDetails.TeamAWin = true;
+        else
+            matchDetails.TeamAWin = false;
+
+        // Date and time
+        const dateTimeContainer = match.querySelector("div.m-item-date");
+        const dateContainer = dateTimeContainer.querySelector("div");
+        matchDetails.Date = getText(dateContainer);
+        dateContainer.remove();
+        matchDetails.Time = getText(dateTimeContainer).replace(/\s+/g, ' ');
+
+        results[i] = matchDetails;
+    }
+    return results;
+}
+
+function getPlayerTeams(teamSections) {
+    let teams = {};
+    
+    // Current team
+    teams.CurrentTeam = processTeamCard(teamSections[0].querySelector("a"));
+
+    // Past teams
+    let pastTeams = {};
+    const pastTeamCards = teamSections[1].querySelectorAll("a");
+    for (let i = 0; i < pastTeamCards.length; i++)
+        pastTeams[i] = processTeamCard(pastTeamCards[i]);
+    teams.PastTeams = pastTeams;
+
+    return teams;
+}
+
+function processTeamCard(teamCard) {
+    let teamInfo = {};
+    const teamID = teamCard.attributes.href
+    teamInfo.TeamID = teamID.split("/")[2];
+    teamInfo.TeamImage = teamCard.querySelector("img").attributes.src;
+    teamInfo.TeamName = getText(teamCard.lastElementChild.firstElementChild);
+    teamInfo.Note = getText(teamCard.lastElementChild.lastElementChild);
+    return teamInfo;
+}
+
+module.exports = { getMainHeaderData, getPlayerAgentStats, getPlayerResults, getPlayerTeams };
--- scrapers/players/players_scraper.js
+++ scrapers/players/players_scraper.js
@@ -1,5 +1,5 @@
 const { getPage, getText } = require("../../utils/scrape_utils");
-const { getMainHeaderData, getPlayerAgentStats } = require("../players/helpers/get_player_information_helper")
+const { getMainHeaderData, getPlayerAgentStats, getPlayerResults, getPlayerTeams } = require("../players/helpers/get_player_information_helper")
 
 async function getPlayerInformation(playerID) {
     const url = `https://vlr.gg/player/${playerID}/?timespan=all`;
@@ -11,6 +11,8 @@ async function getPlayerInformation(playerID) {
         // Get Information From Helpers
         playerInformation.Details = getMainHeaderData(doc.querySelector("div.player-header"));
         playerInformation.AgentStats = getPlayerAgentStats(doc.querySelector("table.wf-table"));
+        playerInformation.Results = getPlayerResults(doc.querySelector("div.player-summary-container-1").children[3]);
+        playerInformation.Teams = getPlayerTeams(doc.querySelector("div.player-summary-container-1").querySelectorAll('div[class="wf-card"]'));
 
         return playerInformation;
     }
