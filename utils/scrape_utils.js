19b8d890d5dad8ba6ba5820e129c375538513db5
BrianJ-4 <brian.joshua03@gmail.com>
2025-05-08 10:34:45 -0700

    Create fetchAndParse

--- utils/scrape_utils.js
+++ utils/scrape_utils.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
 const parser = require("node-html-parser");
 
+async function fetchAndParse(path, func) {
+    const url = `https://vlr.gg${path}`;
+    const doc = await getPage(url);
+    return await func(doc); 
+}
+
 async function getPage(url) {
     const response = await axios.get(url);
     const doc = parser.parse(response.data);
@@ -11,4 +17,4 @@ function getText(element) {
     return element.text.trim();
 }
 
-module.exports = { getPage, getText };
+module.exports = { fetchAndParse, getPage, getText };
